675c78b11a021eeab9eeb3a9bea647eeb6479951
halpo (Andrew Redd) <amredd@gmail.com>
2010-05-08 22:59:56 +0000

    added ruby scripts to build and check
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/osplinebasis@44 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ buildwindows.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env  ruby
+require 'win32/registry'
+
+system "taskkill /F /IM rgui.exe /T"
+
+Dfile =  File.join("pkg","DESCRIPTION")
+description = IO.read(Dfile)
+if description =~ /Package:\s+(\w+)/
+	pkgname = $1
+else
+	raise "Bad description file"
+end
+if description =~ /Version:\s+([\d\.]+)/
+	pkgversion = $1
+else
+	raise "Bad description file"
+end
+
+p pkgname + '_' + pkgversion
+
+system "Rcmd build pkg"
+system "Rcmd INSTALL --build #{pkgname + '_' + pkgversion + '.tar.gz'}"
